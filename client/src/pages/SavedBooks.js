b550cc76c2dfed4caf0b99842c7247a755acdbe3
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-26 22:56:29 -0700

    fixing this tmrw

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -17,81 +17,85 @@ import { useMutation, useQuery } from '@apollo/client';
 // TODO: use GET_ME query instead of `getMe()`
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
   const [removeBook] = useMutation(REMOVE_BOOK);
-  const getMe = useQuery(GET_ME);
+  const { loading, data } = useQuery(GET_ME); // Execute GET_ME query on load
+  const userData = data?.me || {}; // Save query data to userData variable
 
   // TODO: use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-          return false;
-        }
+  //       if (!token) {
+  //         return false;
+  //       }
 
-        const response = await getMe(token);
+  //       const response = await getMe(token);
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
 
-    getUserData();
-  }, [userDataLength]);
+  //   getUserData();
+  // }, [userDataLength]);
+
+
 
   // TODO: create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // const handleDeleteBook = async (bookId) => {
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+  //   if (!token) {
+  //     return false;
+  //   }
 
-    // try {
-    //   // const response = await deleteBook(bookId, token);
-    //   const response = await removeBook({
-    //     variables: { bookId }
-    //   });
+  //   try {
+  //     // const response = await deleteBook(bookId, token);
+  //     const response = await removeBook({
+  //       variables: { bookId }
+  //     });
 
-    //   if (!response.ok) {
-    //     throw new Error('something went wrong!');
-    //   }
+  //     if (!response.ok) {
+  //       throw new Error('something went wrong!');
+  //     }
 
-    //   const updatedUser = await response.json();
-    //   setUserData(updatedUser);
-    //   // upon success, remove book's id from localStorage
-    //   removeBookId(bookId);
-    // } catch (err) {
-    //   console.error(err);
-    // }
+  //     const updatedUser = await response.json();
+  //     setUserData(updatedUser);
+  //     // upon success, remove book's id from localStorage
+  //     removeBookId(bookId);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
 
-    try {
-      // ***** Use the removeBook mutation instead of deleteBook function *****
-      const response = await removeBook({
-        variables: { bookId }
-      });
+  //   // try {
+  //   //   // ***** Use the removeBook mutation instead of deleteBook function *****
+  //   //   const response = await removeBook({
+  //   //     variables: { bookId }
+  //   //   });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+  //   //   if (!response.ok) {
+  //   //     throw new Error('something went wrong!');
+  //   //   }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  //   //   const updatedUser = await response.json();
+  //   //   setUserData(updatedUser);
+  //   //   removeBookId(bookId);
+  //   // } catch (err) {
+  //   //   console.error(err);
+  //   // }
+  // };
 
   // if data isn't here yet, say so
   if (!userDataLength) {
@@ -121,9 +125,9 @@ const SavedBooks = () => {
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    {/* <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
                       Delete this Book!
-                    </Button>
+                    </Button> */}
                   </Card.Body>
                 </Card>
               </Col>
