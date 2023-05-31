1a204d33eb3ad15764c7b033ae6389b2f28ebfeb
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-30 22:52:16 -0700

    updating auth.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,7 +9,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
@@ -23,8 +23,9 @@ const SearchBooks = () => {
 
   // TODO: create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
+  const [saveBook] = useMutation(SAVE_BOOK);
   //TODO:  set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+  
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => {
@@ -33,7 +34,7 @@ const SearchBooks = () => {
   }, [savedBookIds]);
 
     // Use the saveBook mutation to save book 
-    const [saveBook] = useMutation(SAVE_BOOK);
+    // const [saveBook] = useMutation(SAVE_BOOK);
 
   // TODO: create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
