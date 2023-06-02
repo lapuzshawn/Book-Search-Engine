b4ad9674b53b2e1712c769848314093045953a48
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-01 18:22:35 -0700

    updated pkg.json

--- server/server.js
+++ server/server.js
@@ -27,10 +27,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
 /*** Use Apollo Server instead*/
 // db.once('open', () => {
