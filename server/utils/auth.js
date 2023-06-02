a0c46b4ea0f57132fdb1b584eacc3d113d365405
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-01 21:39:39 -0700

    updated auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+/*
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -33,6 +34,50 @@ module.exports = {
     // send to next endpoint
     // next();
     return req;
+  },
+  
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
+*/
+
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function (context) {
+    const { req } = context;
+
+    // allows token to be sent via headers
+    let token = req.headers.authorization;
+
+    if (!token) {
+      throw new Error('You have no token!');
+    }
+
+    // extract the token value
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7);
+    }
+
+    try {
+      // verify token and get user data out of it
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      context.user = data;
+    } catch (err) {
+      console.log('Invalid token');
+      throw new Error('Invalid token!');
+    }
+
+    // continue execution
+    return context;
   },
   
   signToken: function ({ username, email, _id }) {
